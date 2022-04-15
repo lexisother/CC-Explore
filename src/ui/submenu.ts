d3f8821fb532dd9673902ebc334d1aba92eb8b87
Conor Lawton <conor.lawton1@gmail.com>
2022-04-15 16:08:17 +0200

    [ui/submenu] Fix msgBox sizing and clean up

--- src/ui/submenu.ts
+++ src/ui/submenu.ts
@@ -3,28 +3,49 @@ sc.ExploreMenu = sc.BaseMenu.extend({
   buttongroup: null,
   init() {
     this.parent();
-    this.hook.size.x = ig.system.width;
-    this.hook.size.y = ig.system.height;
+    this.setSize(ig.system.width, ig.system.height);
 
-    ig.lang.labels.sc.gui.menu['menu-titles'].explore = 'Explore';
-
-    this.content = new ig.GuiElementBase();
-    this.content.setSize(160, 110);
-
+    // button group stuff
     this.buttonInteract = new ig.ButtonInteractEntry();
     this.buttongroup = new sc.ButtonGroup();
     this.buttonInteract.pushButtonGroup(this.buttongroup);
     this.buttongroup.addPressCallback(this.onBeginButtonPress);
 
-    this.startButton = new sc.ButtonGui('\\i[help2] Start');
-    this.startButton.setAlign(ig.GUI_ALIGN.X_CENTER, ig.GUI_ALIGN.Y_BOTTOM);
-    this.startButton.onButtonPress = this.onBeginButtonPress;
-    let i = 4;
-    while (i-- > 0) this.buttongroup.addFocusGui(this.startButton, i, 1);
-    this.content.addChildGui(this.startButton);
+    // enum manipulation
+    ig.lang.labels.sc.gui.menu['menu-titles'].explore = 'Explore';
 
+    // Wrapper structuring
+    this.content = new ig.GuiElementBase();
     this.msgBox = new sc.CenterBoxGui(this.content);
     this.msgBox.setAlign(ig.GUI_ALIGN.X_CENTER, ig.GUI_ALIGN.Y_CENTER);
+
+    // Textual content sizing
+    let text = new sc.TextGui(
+      'Note: CC-Explore is \\c[1]unfinished\\c[0].\nBug reports can be submitted on the GitHub repository.\nThank you for checking out the mod!',
+    );
+    text.setAlign(ig.GUI_ALIGN.X_CENTER, ig.GUI_ALIGN.Y_TOP);
+    text.setTextAlign(ig.Font.ALIGN.CENTER);
+
+    // Start button
+    this.startButton = new sc.ButtonGui('Start');
+    this.startButton.setAlign(ig.GUI_ALIGN.X_CENTER, ig.GUI_ALIGN.Y_BOTTOM);
+    this.startButton.onButtonPress = this.onBeginButtonPress;
+
+    // Add content
+    this.content.addChildGui(text);
+    this.content.addChildGui(this.startButton);
+
+    // Set wrapper container to fit text content width (ignoring height)
+    let textWidth = text.hook.size.x;
+    let textHeight = text.hook.size.y;
+    this.msgBox.setSize(textWidth + 20, textHeight + 20 + this.startButton.hook.size.y);
+    this.content.setSize(textWidth + 10, textHeight + 10 + this.startButton.hook.size.y);
+
+    // Resize to correct left+right border heights and bg
+    this.msgBox.resize();
+
+    // Add start button
+    this.buttongroup.addFocusGui(this.startButton, 0, 1);
 
     this.addChildGui(this.msgBox);
 
